100d141b9562fab23c524c026f348f6e3194a0f0
ayrah <gojol.ira@wvsu.edu.ph>
2022-12-02 02:16:08 +0845

    added edit delete

--- index.js
+++ index.js
@@ -1,119 +1,356 @@
-const express = require('express')
-const bodyParser = require("body-parser")
-const cors = require("cors")
-const { DB_CONN } = require('./config')
-const { Router } = require('express')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { DB_CONN } = require("./config");
+const { Router } = require("express");
+const app = express();
 
-const PORT = 4001
+const PORT = 4001;
 
-app.use(cors({origin:"*"}))
-app.use(bodyParser.urlencoded({
-    extended:true
-}))
-app.use(bodyParser.json())
+app.use(cors({ origin: "*" }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
-app.get("/", (req, res)=>{
-    res.send("hello irah")
+app.get("/", (req, res) => {
+  res.send("hello irah");
+});
+app.post("/register", (req, res) => {
+  const firstname = req.body.firstname;
+  const middlename = req.body.middlename;
+  const lastname = req.body.lastname;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  let sql =
+    "INSERT INTO instructors (firstname, middlename, lastname, email, password) VALUES(?,?,?,?,?)";
+
+  DB_CONN.query(
+    sql,
+    [firstname, middlename, lastname, email, password],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.sendStatus(500);
+      }
+      console.log(result);
+      if (result.insertId)
+        return res.send({ success: true, msg: "Registration Success!" });
+      return res.send({ success: false, msg: "Registration Failed!" });
+    }
+  );
+});
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  console.log("login data:", { email, password });
+  const sql = "SELECT * FROM instructors WHERE email = ?";
+
+  DB_CONN.query(sql, email, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    if (result.length === 0)
+      return res.send({ success: false, msg: "wrong email" });
+
+    if (result[0].password !== password)
+      return res.send({ success: false, msg: "Password Incorrect!" });
+
+    return res.send({ success: true, msg: "Login Success!", data: result[0] });
+  });
+});
+app.post("/deleteStudent",(req, res)=>{
+  const studentId = req.body.studentId
+
+  let sql = "DELETE FROM students WHERE student_id = ?"
+
+  DB_CONN.query(sql,studentId,(err, result)=>{
+    if(err){
+      res.send({success:false})
+      throw err
+    }
+    res.send({success:true})
+  })
 })
+app.post("/deleteClass",(req, res)=>{
+  const id = req.body.id
+  console.log(id)
 
-app.post("/login", (req, res)=>{
-    const email = req.body.email
-    const password = req.body.password
+  let sql = "DELETE FROM subjects WHERE id = ?"
 
-    console.log("login data:",{email, password})
-    const sql = "SELECT * FROM accounts WHERE email = ?"
+  DB_CONN.query(sql,id,(err, result)=>{
+    if(err){
+      res.send({success:false})
+      throw err
+    }
+    console.log(result)
+    res.send({success:true})
+  })
+})
+app.post("/unenroll",(req, res)=>{
+  const classId = req.body.classId
+  const  studentId = req.body.studentId
 
-    DB_CONN.query(sql,email,(err, result)=>{
+  let sql = "DELETE FROM classes WHERE course_number = ? and student_id = ?"
+
+  DB_CONN.query(sql,[classId, studentId],(err, result)=>{
+    if(err){
+      res.send({success:false})
+      throw err
+    }
+    console.log(result)
+    res.send({success:true})
+  })
+})
+app.get("/getClasses/:email", (req, res) => {
+  const email = req.params.email;
+
+  const sql = "SELECT subjects.*, instructors.firstname, instructors.middlename, instructors.lastname FROM subjects INNER JOIN instructors ON subjects.instructor = instructors.email WHERE subjects.instructor = ? GROUP BY subjects.id"
+  DB_CONN.query(sql, email, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+    console.log(result);
+    res.send(result);
+  });
+});
+app.post("/addClass", (req, res) => {
+  const courseNumber = req.body.courseNumber;
+  const courseTitle = req.body.courseTitle;
+  const semester = req.body.semester;
+  const schoolYear = req.body.schoolYear;
+  const email = req.body.email;
+
+  let sql =
+    "INSERT INTO subjects (course_title, course_number, semester, school_year, instructor) VALUES(?,?,?,?,?)";
+
+  DB_CONN.query(
+    sql,
+    [courseTitle,courseNumber ,semester,schoolYear, email],
+    (err1, result) => {
+      if (err1) {
+        console.log(err1);
+        return res.send({ success: false, msg: "Error adding class" });
+      }
+      console.log(result);
+      let classSql = " SELECT subjects.*, instructors.firstname, instructors.middlename, instructors.lastname FROM subjects INNER JOIN instructors ON subjects.instructor = instructors.email WHERE subjects.id = ?  GROUP BY subjects.id"
+      
+      DB_CONN.query(classSql, [result.insertId],(err, result1)=>{
         if(err){
-            console.log(err)
-            return res.sendStatus(500)
+          console.log(err)
+          return res.send({success:false, msg:"Database error"})
         }
-        if(result.length === 0)return res.send({success:false, msg:"wrong email"})
-        
-        if(result[0].password !== password)return res.send({success:false, msg:"Password Incorrect!"})
+        res.send({success:true, data:result1[0]})
+      })
+    }
+  );
+});
+app.post("/editClass/:id", (req, res) => {
+  const courseNumber = req.body.courseNumber;
+  const courseTitle = req.body.courseTitle;
+  const semester = req.body.semester;
+  const schoolYear = req.body.schoolYear;
+  const email = req.body.email;
 
-        return res.send({success:true, msg:"Login Success!"})
+  const id = req.params.id
 
-    })
-})
-app.get("/getClasses", (req, res)=>{
+  let sql =
+    "UPDATE subjects SET course_title=?, course_number=?, semester=?, school_year=?, instructor=? WHERE id=?"
 
-    const sql = "SELECT * FROM class"
-    DB_CONN.query(sql, (err, result)=>{
-        if(err){
-            console.log(err)
-            res.sendStatus(500)
-        }
-        console.log(result)
-        res.send(result)
-    })
-})
-app.post("/addClass", (req, res)=>{
-    const courseNum = req.body.courseNum
-    const courseTitle = req.body.courseTitle
-    const firstname = req.body.firstname
-    const middlename = req.body.middlename
-    const lastname = req.body.lastname
+  DB_CONN.query(
+    sql,
+    [courseTitle,courseNumber ,semester,schoolYear, email, id],
+    (err1, result) => {
+      if(err1){
+        res.send({succes:false})
+        throw err1
+      }
+      res.send({success:true})
 
-    let sql1 = "INSERT INTO instructors(firstname, middlename, lastname) VALUES(?,?,?)"
+    }
+  );
+});
+app.post("/getAttendanceToday", (req, res) => {
+  const classId = req.body.classId;
+  sql =
+    "SELECT students.* FROM `attendance` INNER JOIN students ON students.student_id = attendance.student_id WHERE attendance.course_id = ?";
+  DB_CONN.query(sql, classId, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+    console.log(" result", result);
+    res.send(result);
+  });
+});
 
-    DB_CONN.query(sql1,[firstname, middlename, lastname],(err, result)=>{
-        if(err){
-            console.log(err)
-            res.send(500)
-        }
-        if(!result.insertId)return res.send({success:false, msg:"Error adding class"})
+app.post("/addStudent", (req, res) => {
+  const firstname = req.body.firstname;
+  const yearSection = req.body.courseYearSec;
+  const id = req.body.studentId.toUpperCase();
+  const middlename = req.body.middlename;
+  const lastname = req.body.lastname;
+  const gender = req.body.gender
 
-        let sql = "INSERT INTO class (course_title, course_number, instructor) VALUES(?,?,?)"
+  let sql =
+    "INSERT INTO students (student_id, firstname, middlename, lastname,gender, year_section) VALUES(?,?,?,?,?,?)";
 
-        DB_CONN.query(sql, [courseNum, courseTitle, result.insertId], (err1, result1)=>{
-            if(err1) {
-                console.log(err1)
-                return res.send({success:false, msg:"Error adding class"})
-            }
-            console.log(result)
-            res.send({success:true, msg:"Class added"})
-        })
+  DB_CONN.query(
+    sql,
+    [id, firstname, middlename, lastname,gender, yearSection],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        if (err.errno === 1062)
+          return res.send({ success: false, code: "duplicate" });
+        return res.send({ success: false, msg: "Error adding student" });
+      }
+      console.log(result);
+      if (result.insertId) return res.send({ success: true, code: "" });
+      res.send({ success: false, code: "" });
+    }
+  );
+});
+app.post("/getStudents", (req, res) => {
+  const classId = req.body.classId;
 
-    })
+  const sql = "SELECT students.* FROM students INNER JOIN classes ON classes.student_id = students.student_id WHERE classes.course_number = ?";
+  DB_CONN.query(sql, classId, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+    console.log(result);
+    res.send(result);
+  });
+});
+app.post("/getAllStudents", (req, res) => {
+  const classId = req.body.classId;
 
-})
-
-app.post("/addStudent", (req, res)=>{
-    const id = req.body.id
-    const firstname = req.body.firstname
-    const yearSection = req.body.yearSection
-    const classId = req.body.classId
-    const middlename = req.body.middlename
-    const lastname = req.body.lastname
+  const sql = "SELECT * FROM students";
+  DB_CONN.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+    console.log(result);
+    res.send(result);
+  });
+});
+app.get("/searchStudent/:toSearch", (req, res) => {
+    const toSearch = "%"+req.params.toSearch+"%";
     
-    let sql = "INSERT INTO students (student_id, firstname, middlename, lastname, year_section, class_id) VALUES(?,?,?,?,?,?)"
-
-    DB_CONN.query(sql, [id, firstname, middlename, lastname, yearSection, classId], (err, result)=>{
-        if(err) {
-            console.log(err)
-            return res.send({success:false, msg:"Error adding student"})
-        }
-        console.log(result)
-        res.send({success:true, msg:"Student added"})
-    })
-})
-app.post("/getStudents", (req, res)=>{
+  
+    const sql = 'SELECT CONCAT(firstname , " ", middlename, " ", lastname )as fullname, student_id, year_section FROM `students` WHERE CONCAT(firstname , " ", middlename, " ", lastname) LIKE ?';
+    DB_CONN.query(sql, toSearch,(err, result) => {
+      if (err) {
+        console.log(err);
+        res.sendStatus(500);
+      }
+      console.log(result);
+      res.send(result);
+    });
+  });
+app.post("/enroll", (req, res)=>{
+    const studentId = req.body.studentId
     const classId = req.body.classId
+    const sql = "INSERT INTO classes(course_number, student_id) VALUES(?,?)"
 
-    const sql = "SELECT * FROM students WHERE class_id = ?"
-    DB_CONN.query(sql,classId, (err, result)=>{
+    DB_CONN.query(sql, [classId,studentId],(err, result)=>{
         if(err){
             console.log(err)
-            res.sendStatus(500)
+            return res.send({success:false, msg:"Error"})
+        }
+        console.log(result)
+        res.send({success:true})
+    })
+})
+app.post("/checkAttendace", (req, res) => {
+  const studentId = req.body.id;
+  const classId = req.body.classId;
+  console.log(studentId, classId);
+
+  function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
+    const dateNow = new Date()
+    const dateString = formatDate(dateNow)
+    console.log(dateString)
+
+  let checkDup = "SELECT COUNT(id)as count FROM attendance WHERE date ? AND student_id = ? AND course_id = ?"
+
+    let sql = "SELECT * FROM students INNER JOIN classes on students.student_id = classes.student_id WHERE classes.course_number = ? AND classes.student_id = ?";
+
+    DB_CONN.query(sql, [classId, studentId], (err, result) => {
+      if (err) {
+        console.log(err);
+        res.sendStatus(500);
+      }
+      if (result.length === 0)
+        return res.send({ success: false, msg: "no data" });
+      sql = "INSERT INTO attendance (date, course_id, student_id) VALUES (?,?,?)";
+
+  
+      DB_CONN.query(
+        sql,
+        [dateString, classId, studentId],
+        (err1, result1) => {
+          if (err) {
+            console.log(err1);
+            res.sendStatus(500);
+          }
+        }
+      );
+      res.send({ success: true, result });
+    });
+  })
+
+app.post("/getAttendance",(req, res)=>{
+    const classId = req.body.classId
+
+    const sql = "SELECT date_format(date, '%m-%d-%Y') as date, COUNT(student_id) as present  FROM attendance WHERE course_id = ? GROUP BY date ORDER BY date"
+
+    DB_CONN.query(sql, classId,(err, result)=>{
+        if(err){
+            console.log(err)
+            return res.send({success:false, msg:"Database Error"})
         }
         console.log(result)
         res.send(result)
     })
 })
-app.listen(PORT,()=>{
-    console.log("Server is running in port"+PORT)
-})
+app.post("/attendanceDetails",(req, res)=>{
+    const date = req.body.date
 
-module.exports = app
+    const sql = "SELECT students.* FROM students INNER JOIN attendance ON students.student_id = attendance.student_id WHERE date_format(attendance.date, '%m-%d-%Y') = ?"
+    DB_CONN.query(sql, date,(err, result)=>{
+        if(err){
+            console.log(err)
+            return res.send({success:false, msg:"Database Error"})
+        }
+        console.log(result)
+        res.send(result)
+    })
+})
+app.listen(PORT, () => {
+  console.log("Server is running in port" + PORT);
+});
+
+module.exports = app;
